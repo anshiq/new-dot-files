11543547b6c96bd45e8be6c7664f5b25685b1cdb
anshiq <anshikthind@gmail.com>
2023-07-18 20:03:08 +0530

    initialize

--- dotfilesdata/install.sh
+++ dotfilesdata/install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
-sudo apt install build-essential tmux cmake git npm python3-pip curl gedit  && sudo pacman -S base-devel tmux cmake neovim gedit python-pip npm  git 
+sudo apt update && sudo apt upgrade && sudo pacman -Syyu
+sudo apt install build-essential tmux cmake git xclip npm python3-pip curl gedit  && sudo pacman -S base-devel tmux cmake neovim gedit python-pip npm  git xclip 
 git clone https://github.com/jimeh/tmuxifier.git ~/.tmuxifier
-#curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 mkdir -p ~/.local/share/fonts
 cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
 fc-cache -f -v
+sudo npm i -g n
 mkdir "${HOME}/.npm-packages"
 npm config set prefix "${HOME}/.npm-packages"
 curl -fsSL https://bun.sh/install | bash
@@ -13,3 +15,4 @@ sudo rm /usr/lib/python3.11/EXTERNALLY-MANAGED
 rm ~/.zshrc
 ln -p ./.zshrc ~/.zshrc
 LV_BRANCH='release-1.3/neovim-0.9' bash <(curl -s https://raw.githubusercontent.com/LunarVim/LunarVim/release-1.3/neovim-0.9/utils/installer/install.sh)
+cat ./setupCommands.txt
